c5a248dd7ef9f44119ea5ea77fa8658db1ac9d13
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-08 17:32:47 +0100

    ADD:milestone fine 2

--- js/app.js
+++ js/app.js
@@ -7,16 +7,43 @@
 
 // creo array 
 const arrayStringImg = ['./img/01.webp', './img/02.webp', './img/03.webp', './img/04.webp', './img/05.webp']
+//creao collegamento con elemento DOM dove andrò ad inserire al suo interno 
 
+
+ //  MILESTONE 3
+// Al click dell'utente sulle frecce, il programma cambierà l’immagine
+//  attiva, che quindi verrà visualizzata al posto della precedente.
 let imagesSlider = document.querySelector('.slider-img')
 for(let i = 0; i < arrayStringImg.length; i++){
     let imageString = arrayStringImg[i]
-    
 
     imagesSlider.innerHTML += 
-    `<div class="item active">
+    `<div class="item">
           <img src="${imageString}" alt="">
      </div>
     `
 
+    
 }
+
+const activeImg = document.getElementsByClassName('item'); //array collection
+    activeImg[0].classList.add('active')
+
+
+let next = document.querySelector('.next')
+    next.addEventListener('click', function(){
+        for(let i = 0; i < activeImg.length; i++){
+            let activeImgIndex = activeImg[i]
+            activeImgIndex.classList.add('active')
+        }
+    })
+    
+
+ 
+
+
+
+
+
+
+
